2ac260e4f20b78bf80aea8e85c5f9a5ea7ee9d9f
Đỗ Đào Trúc Quyên <113251436+quyenyeu147@users.noreply.github.com>
2022-09-11 00:04:52 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/assignment/QLTKActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QLTKActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quan_ly_tk);
+    }
+}
